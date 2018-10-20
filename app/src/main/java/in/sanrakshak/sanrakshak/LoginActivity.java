ff6b8b7656a9afd2dbc45f62b3e1cdb7d5be12b7
Ritik Srivastava <ritik.space@gmail.com>
2018-10-20 21:12:58 +0530

    Login Assets

--- app/src/main/java/in/sanrakshak/sanrakshak/LoginActivity.java
+++ app/src/main/java/in/sanrakshak/sanrakshak/LoginActivity.java
@@ -81,12 +81,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-
-
         splash_cover=findViewById(R.id.splash_cover);
         ico_splash=findViewById(R.id.ico_splash);
         logo_div=findViewById(R.id.logo_div);
