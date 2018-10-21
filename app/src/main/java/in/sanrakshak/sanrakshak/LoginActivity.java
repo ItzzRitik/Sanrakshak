02459c8fb82a976eadb61e613e63e7992d55f144
Ritik Srivastava <ritik.space@gmail.com>
2018-10-21 23:14:30 +0530

    Login User Interface

--- app/src/main/java/in/sanrakshak/sanrakshak/LoginActivity.java
+++ app/src/main/java/in/sanrakshak/sanrakshak/LoginActivity.java
@@ -69,6 +69,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
         splash_cover=findViewById(R.id.splash_cover);
         ico_splash=findViewById(R.id.ico_splash);
         logo_div=findViewById(R.id.logo_div);
