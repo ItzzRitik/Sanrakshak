d6a6e48561c5c92779b345862411e5cbae1849e2
Ritik Srivastava <ritik.space@gmail.com>
2018-12-03 23:05:55 +0530

    Homepage Development - Setting Cracks List

--- app/src/main/java/in/sanrakshak/sanrakshak/Cracks.java
+++ app/src/main/java/in/sanrakshak/sanrakshak/Cracks.java
@@ -3,15 +3,18 @@ package in.sanrakshak.sanrakshak;
 import android.graphics.Bitmap;
 
 public class Cracks {
-    private String name,date;
+    private String name,intensity,date;
     private Bitmap preview;
-    Cracks(String name, String date, Bitmap preview) {
+    Cracks(String name,String intensity, String date, Bitmap preview) {
         this.name = name;
+        this.intensity = intensity;
         this.date = date;
         this.preview = preview;
     }
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
+    public String getIntensity() {return intensity;}
+    public void setIntensity(String intensity) {this.intensity = intensity;}
     public String getDate() {return date;}
     public void setDate(String date) {this.date = date;}
     public Bitmap getPreview() {return preview;}
