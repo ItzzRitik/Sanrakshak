735a7a39edde72342cfa771a66ab26ab9edb7dd3
Ritik Srivastava <ritik.space@gmail.com>
2018-12-12 03:28:58 +0530

    Profile Page

--- app/src/main/java/in/sanrakshak/sanrakshak/HomeActivity.java
+++ app/src/main/java/in/sanrakshak/sanrakshak/HomeActivity.java
@@ -147,19 +147,19 @@ public class HomeActivity extends AppCompatActivity {
         menu.setOnClickListener(v -> {
             if(!menuOpen){
                 menuOpen=true;
-                menu.animate().rotationBy(1080).withEndAction(null).setDuration(400).setInterpolator(new AccelerateDecelerateInterpolator()).start();
+                menu.animate().rotationBy(720).withEndAction(null).setDuration(200).setInterpolator(new AccelerateDecelerateInterpolator()).start();
                 new Handler().postDelayed(() -> {
                     menu.setPadding(2,2,2,2);
                     menu.setImageDrawable(getDrawable(R.drawable.close));
-                },200);
+                },100);
             }
             else{
                 menuOpen=false;
-                menu.animate().rotationBy(1080).withEndAction(null).setDuration(400).setInterpolator(new AccelerateDecelerateInterpolator()).start();
+                menu.animate().rotationBy(720).withEndAction(null).setDuration(200).setInterpolator(new AccelerateDecelerateInterpolator()).start();
                 new Handler().postDelayed(() -> {
                     menu.setPadding(0,0,0,0);
                     menu.setImageDrawable(getDrawable(R.drawable.menu));
-                },200);
+                },100);
             }
         });
         done=findViewById(R.id.done);
